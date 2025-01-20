6ab3f6ca96bc2e73fa2ae1e4aaa0c620b3b2e4a7
SSmith <stephensmith@live.ie>
2025-01-20 14:18:33 +0000

    Update config_file_creator.py

--- config_file_creator.py
+++ config_file_creator.py
@@ -1,15 +1,3 @@
-#!/bin/sh 
-#SBATCH --job-name="novo"
-#SBATCH -o /data/ssmith/logs/novo_%A_%a.out
-#SBATCH -e /data/ssmith/logs/novo_%A_%a.err
-
-source /home/ssmith/.bashrc
-source activate novo_env
-
-sample_name=$(basename /data2/ssmith/fastqs/"$SLURM_ARRAY_TASK_ID"_*_1.fq.gz _1.fq.gz)
-
-NOVOPlasty.pl -c /data2/ssmith/novo_config_files/"$sample_name"_config.txt
-[ssmith@lugh ssmith]$ more /data/ssmith/scripts/colony_analysis/jan_analysis/mito_analysis/config_file_creator.py 
 #Creating contig files for multiple NOVOplasty runs
 
 import os
